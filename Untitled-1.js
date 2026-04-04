d6a5b3204a2b332acca549d8990d55d34d9181ff
kashamrokaya-cyber <kashamrokaya18@gmail.com>
2026-04-04 16:38:35 +0545

    source

---
+++ Untitled-1.js
@@ -0,0 +1,6 @@
+// 127.0.0.1:3000 we can use insted of localhost:3000,
+// [::1]:3000  insted of localhost:3000
+
+// always we can  request  (get) element of http  in browser not post, patch, update
+
+// always we send data in the form of JSON format ({...}); not string like res.send("mern")
--- app.js
+++ app.js
@@ -14,6 +14,11 @@ app.get("/about",(req,res)=>{
         message:"hello i am from about api"
     })
 })
+app.get("/source",(req,res)=>{
+    res.json({
+        message:"i ma source"
+    })
+})
 
 app.listen(3000,()=>{ //total port number limited 0-65535 adn 0-1000 not good for port because it can be used for ownself.
     console.log("listening 3000");
